16ed8581137202bec25ace3b569d3dde63a75626
Glitch (androz2091-giveaways-bot-2) <none>
2020-02-08 21:16:55 +0000

    😼🐥 Updated with Glitch

---
+++ commands/invite.js
@@ -0,0 +1,6 @@
+const ms = require('ms');
+
+exports.run = async (client, message, args) => {
+  message.channel.send("Invite me at: https://discordapp.com/api/oauth2/authorize?client_id=668889051096678420&permissions=8&scope=bot")
+
+};
